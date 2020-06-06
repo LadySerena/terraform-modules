621ebca587415261b996401d4430bd8999c3edd6
LadySerena <serena.tiede+github@gmail.com>
2020-06-06 18:25:59 -0500

    feat: add github+cloudbuild module
    This will allow me to create github repos and their corresponding cloud
    build triggers.

---
+++ githubWithCloudBuild/main.tf
@@ -0,0 +1,37 @@
+resource "github_repository" "githubRepo" {
+  name = var.repoName
+  description = var.description
+  private = var.private
+  auto_init = var.autoInit
+  license_template = var.license
+
+}
+
+resource "google_cloudbuild_trigger" "masterTrigger" {
+  name = "${github_repository.githubRepo.name}-master"
+  description = "ci for master branch for ${github_repository.githubRepo.html_url}"
+  filename = var.ciMasterPath
+  ignored_files = var.ignoredFiles
+  github {
+    owner = split("/", github_repository.githubRepo.full_name)[0]
+    name = split("/", github_repository.githubRepo.full_name)[1]
+    push {
+      branch = "master"
+    }
+  }
+}
+
+resource "google_cloudbuild_trigger" "devTrigger" {
+  name = "${github_repository.githubRepo.name}-dev"
+  description = "ci for dev branches for ${github_repository.githubRepo.html_url}"
+  filename = var.ciDevPath
+  ignored_files = var.ignoredFiles
+  github {
+    owner = split("/", github_repository.githubRepo.full_name)[0]
+    name = split("/", github_repository.githubRepo.full_name)[1]
+    pull_request {
+      branch = ".*"
+      comment_control = "COMMENTS_ENABLED"
+    }
+  }
+}
