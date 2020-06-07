6d181d631f0e3f37f04dda19c7977cbd19607131
LadySerena <serena.tiede+github@gmail.com>
2020-06-06 21:11:13 -0500

    fix: remove github from module
    the github terraform provider doesn't handle personal
    repos.

--- githubWithCloudBuild/main.tf
+++ githubWithCloudBuild/main.tf
@@ -1,12 +1,12 @@
 resource "google_cloudbuild_trigger" "masterTrigger" {
   provider = google-beta
   name = "${var.repoName}-master"
-  description = "ci for master branch for ${github_repository.githubRepo.html_url}"
+  description = "ci for master branch for https://github.com/${var.ownerName}/${var.repoName}"
   filename = var.ciMasterPath
   ignored_files = var.ignoredFiles
   github {
-    owner = split("/", github_repository.githubRepo.full_name)[0]
-    name = split("/", github_repository.githubRepo.full_name)[1]
+    owner = var.ownerName
+    name = var.repoName
     push {
       branch = "master"
     }
@@ -16,12 +16,12 @@ resource "google_cloudbuild_trigger" "masterTrigger" {
 resource "google_cloudbuild_trigger" "devTrigger" {
   provider = google-beta
   name = "${var.repoName}-dev"
-  description = "ci for dev branches for ${github_repository.githubRepo.html_url}"
+  description = "ci for pull requests for https://github.com/${var.ownerName}/${var.repoName}"
   filename = var.ciDevPath
   ignored_files = var.ignoredFiles
   github {
-    owner = split("/", github_repository.githubRepo.full_name)[0]
-    name = split("/", github_repository.githubRepo.full_name)[1]
+    owner = var.ownerName
+    name = var.repoName
     pull_request {
       branch = ".*"
       comment_control = "COMMENTS_ENABLED"
--- githubWithCloudBuild/variables.tf
+++ githubWithCloudBuild/variables.tf
@@ -6,28 +6,6 @@ variable "ownerName" {
   type = string
 }
 
-variable "description" {
-  type = string
-}
-
-variable "private" {
-  type = bool
-  default = false
-  description = "should this github repo be private? defaults to false"
-}
-
-variable "autoInit" {
-  type = bool
-  default = false
-  description = "should github create the initial commit and readme? defaults to false"
-}
-
-variable "license" {
-  type = string
-  default = "apache-2.0"
-  description = "which license should be attached to this project (defaults to apache 2.0). see https://www.terraform.io/docs/providers/github/r/repository.html#license_template for more information"
-}
-
 variable "ciMasterPath" {
   type = string
   default = "ci/master/cloudbuild.yaml"
@@ -40,5 +18,7 @@ variable "ciDevPath" {
 
 variable "ignoredFiles" {
   type = list(string)
-  default = ["README.md", ".gitignore"]
+  default = [
+    "README.md",
+    ".gitignore"]
 }
