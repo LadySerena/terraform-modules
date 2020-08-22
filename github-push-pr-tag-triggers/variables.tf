7aed94d7d6f29048e26f56fa51caae5866c7212e
LadySerena <serena.tiede+github@gmail.com>
2020-08-22 17:30:18 -0500

    feat: add new module for tag based ci
    This module lets someone build pushes to main and prs with the same
    cloud build config and another cloud build config for tags.

---
+++ github-push-pr-tag-triggers/variables.tf
@@ -0,0 +1,32 @@
+variable "repoName" {
+  type = string
+}
+
+variable "ownerName" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+variable "ciMainPath" {
+  type = string
+  default = "ci/main/cloudbuild.yaml"
+}
+
+variable "ciDevPath" {
+  type = string
+  default = "ci/feature/cloudbuild.yaml"
+}
+
+variable "ignoredFiles" {
+  type = list(string)
+  default = [
+    "README.md",
+    ".gitignore"]
+}
+
+variable "project_id" {
+  type = string
+}
